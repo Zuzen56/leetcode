6991296c500cd0efdae6c34284380c9f9e0f46d0
Zuzen <zhuzhan51@gmail.com>
2024-07-19 21:45:28 +0800

    7-19

--- 263.py
+++ 263.py
@@ -1,16 +1,10 @@
 def function(n):
-    while n > 1 and n != 1:
-        while n > 1 and n != 1:
-            while n > 1 and n != 1:
-                if n % 2 != 0:
-                    return bool(0)
-                n //= 2
-            if n % 3 != 0:
-                return bool(0)
-            n //= 3
-        if n % 5 != 0:
-            return bool(0)
-        n //= 5
-    return bool(1) and n > 0
+    if n <= 0:
+        return False
+    s = [2,3,5]
+    for i in s :
+        while n % i == 0:
+            n //= i
+    return n == 1
 
-print(function(100))
+print(function(6))
