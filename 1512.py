cb4e651edd0c7fc7d265919e8db1cc170d37d717
Zuzen <zhuzhan51@gmail.com>
2024-07-21 23:17:25 +0800

    7-21

--- 1512.py
+++ 1512.py
@@ -10,4 +10,16 @@ def numIdenticalPairs( nums):
         j = i + 1
     return result
 
+def countGoodTriplets( arr, a, b, c):
+    nums = 0
+    for i in range(len(arr) - 2):
+        j = i + 1
+        for j in range(i + 1 ,len(arr) -1):
+            k = j + 1
+            for k in range(j + 1 ,len(arr)):
+                if abs(arr[i] - arr[j]) <= a and abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
+                            nums += 1
+    return nums
+
+
 print(numIdenticalPairs([1,1,1,1]))
--- 1534.py
+++ 1534.py
@@ -1,14 +1,17 @@
 def function(array,a,b,c):
     length = len(array)
     nums = 0
-    for i in length:
-        for j in length:
-            for k in length:
+    for i in range(length - 2):
+        j = i + 1
+        while j < length -1:
+            k = j + 1
+            while k < length:
                 if abs(array[i] - array[j]) <= a:
                     if abs(array[j] - array[k]) <= b:
                         if abs(array[i] - array[k]) <= c:
                             nums += 1
-
+                k += 1
+            j += 1
     return nums
 
 print(function([3,0,1,1,9,7],7,2,3))
