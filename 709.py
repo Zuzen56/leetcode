6672587fd08474c407d45e1af62677b0ba48e961
Zuzen <zhuzhan51@gmail.com>
2024-07-22 22:53:03 +0800

    7-22

--- 709.py
+++ 709.py
@@ -1,4 +1,18 @@
 def function(s):
-    return s
+    string = ""
+    for i in s:
+        i = ord(i)
+        i = int(i)
+        if i<= 90 and i >=65:
+            i = i + 32
+            i = chr(i)
+            string = string + i
+        else:
+            i = chr(i)
+            string = string + i
+    return string
 
-print(function("ILoveYou"))
+#def function(s):
+#    return s.lower()
+
+print(function("loWERcAse"))
